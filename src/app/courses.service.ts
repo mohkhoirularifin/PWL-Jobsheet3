7d8231d871a3e74cff0d181a5434e826be528ffd
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-02-13 21:31:00 +0700

    Update courses.service.ts

--- src/app/courses.service.ts
+++ src/app/courses.service.ts
@@ -6,4 +6,13 @@ import { Injectable } from '@angular/core';
 export class CoursesService {
 
   constructor() { }
+  getCourses(){
+    return [
+    {id: 0, name: 'HTML'},
+    {id: 1, name: 'PHP'},
+    {id: 2, name: 'ANGULAR'},
+    {id: 3, name: 'C#'},
+    {id: 4, name: 'VB.NET'},
+    ];
+  }
 }
