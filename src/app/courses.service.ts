6c105b4737330c6a69bcc04595deaaeebd3b1930
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-02-13 22:45:06 +0700

    Tugas 1

--- src/app/courses.service.ts
+++ src/app/courses.service.ts
@@ -8,11 +8,14 @@ export class CoursesService {
   constructor() { }
   getCourses(){
     return [
-    {id: 0, name: 'HTML'},
-    {id: 1, name: 'PHP'},
-    {id: 2, name: 'ANGULAR'},
-    {id: 3, name: 'C#'},
-    {id: 4, name: 'VB.NET'},
+    {name: 'Author1'},
+    {name: 'Author2'},
+    {name: 'Author3'},
+    // {id: 0, name: 'HTML'},
+    // {id: 1, name: 'PHP'},
+    // {id: 2, name: 'ANGULAR'},
+    // {id: 3, name: 'C#'},
+    // {id: 4, name: 'VB.NET'},
     ];
   }
 }
